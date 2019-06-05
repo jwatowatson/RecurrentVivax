5a41a41a714cf9def9e9be50407910f5cdf6fc18
Aimee R. Taylor <aimee.r.taylor.85@gmail.com>
2019-06-05 12:44:37 -0400

    Inspecting impact of recrudescence frailty when probabilistically phasing

--- Pooled_Final_Analysis/Effect_probablistic_v_deterministic.R
+++ Pooled_Final_Analysis/Effect_probablistic_v_deterministic.R
@@ -15,9 +15,11 @@ source("../Genetic_Model/post_prob_CLI.R")
 source("../Genetic_Model/test_Rn_compatible.R") 
 source("../Genetic_Model/Data_Inflation_Functions.R")
 source("../Genetic_Model/hap_combinations.R")
+source("../Genetic_Model/PlottingFunction.R")
 load('../RData/Data_for_relatedness.RData') # For Fs_combined
 load('../RData/GeneticModel/MS_data_PooledAnalysis.RData') # Pooled MS data from BPD and VHX
 RUN = F
+MSs = names(Fs_Combined)
 
 #=====================================================
 # Star with simple cases with one or two recurrence
@@ -26,6 +28,8 @@ if(RUN){ # Run models
   Results_MaxHap0 = post_prob_CLI(MSdata = MS_pooled, Fs = Fs_Combined, verbose = T, Max_Hap_genotypes = 0)
   Results_MaxHap20 = post_prob_CLI(MSdata = MS_pooled, Fs = Fs_Combined, verbose = T, Max_Hap_genotypes = 20)
   #save(Results_MaxHap0, Results_MaxHap20, file = 'Results_prob_v_det_simple.RData')
+} else {
+  load('Results_prob_v_det_simple.RData')
 }
 
 #============================================
@@ -42,14 +46,14 @@ if(RUN){ # Run models
   Results_inflate_MaxHap0 = post_prob_CLI(MSdata = MS_inflated, Fs = Fs_Combined, verbose = T, Max_Hap_genotypes = 0)
   Results_inflate_MaxHap20 = post_prob_CLI(MSdata = MS_inflated, Fs = Fs_Combined, verbose = T, Max_Hap_genotypes = 20)
   #save(Results_inflate_MaxHap0, Results_inflate_MaxHap20, file = 'Results_inflate_prob_v_det_simple.RData')
+} else {
+  load('Results_inflate_prob_v_det_simple.RData')
 }
 
 
 #============================================
 # Now plot results
 #============================================
-load('Results_prob_v_det_simple.RData')
-load('Results_inflate_prob_v_det_simple.RData')
 
 # Check all the same
 all(rownames(Results_MaxHap0) == rownames(Results_MaxHap20)) 
@@ -70,8 +74,8 @@ lapply(Rownames, function(X){
 
 
 # Extract comparisons usign probablistic vs deterministic approach
-inds_to_compare = which(Results_MaxHap0$Phased == 'P_P' & Results_MaxHap20$Phased == "D_D")
-inds_to_compare_inflate = which(Results_inflate_MaxHap0$Phased == 'P_P' & Results_inflate_MaxHap20$Phased == "D_D")
+inds_to_compare = (Results_MaxHap0$Phased == 'P_P' & Results_MaxHap20$Phased == "D_D")
+inds_to_compare_inflate = (Results_inflate_MaxHap0$Phased == 'P_P' & Results_inflate_MaxHap20$Phased == "D_D")
 
 # Plot
 par(mfrow = c(3,2))
@@ -87,4 +91,61 @@ for(state in c("C","L","I")){
   abline(a = 0, b = 1)
 }
 
+#===========================================================
+# When Max_Hap_genotypes = 20, how many are probablistically
+# phased, thus potentially impacted by recrudescene frailty
+#===========================================================
+# First let's look at those that were not deterministically phased
+Results_MaxHap20[Results_MaxHap20$Phased != 'D_D', ] # 5 different IDs
+Results_inflate_MaxHap20[Results_inflate_MaxHap20$Phased != 'D_D', ] 
 
+# Extract rownames (!= episode name for inflated)
+rownames_simple_prob = rownames(Results_MaxHap20)[Results_MaxHap20$Phased != 'D_D']
+rownames_inflate_prob = rownames(Results_inflate_MaxHap20)[Results_inflate_MaxHap20$Phased != 'D_D']
+
+# Extract episode names
+Episodes_prob_simple = unique(rownames_simple_prob[!is.na(rownames_simple_prob)])
+Episodes_prob_inflate = unique(sapply(strsplit(rownames_inflate_prob[!is.na(rownames_inflate_prob)], 
+                                               split = "%"), function(x){
+                                                 ID = gsub('TID', '', x[1])
+                                                 episode = do.call(rbind, strsplit(x[5], split = "_"))[,2]
+                                                 paste(ID, episode, sep = '_')
+                                               }))
+
+IDs_prob_simple = apply(do.call(rbind, strsplit(Episodes_prob_simple, '_'))[,1:2], 1, paste, collapse = "_")
+IDs_prob_inflate = apply(do.call(rbind, strsplit(Episodes_prob_inflate, '_'))[,1:2], 1, paste, collapse = "_")
+
+# Extract IDs
+Episodes_prob = c(Episodes_prob_simple, Episodes_prob_inflate)
+IDs_prob = c(IDs_prob_simple, IDs_prob_inflate)
+
+# How many IDs might be impacted? 
+length(IDs_prob) # 24 IDs
+mean(unique(MS_pooled$ID) %in% IDs_prob)*100 # 8%
+MS_pooled_prob = MS_pooled[MS_pooled$ID %in% IDs_prob,]
+
+# COI patterns and number of markers typed of those potentially impacted:
+# most have some potential for recrudescence (decreasing diversity)
+# all very high number of markers, so very potentially impacted
+ddply(MS_pooled_prob, .variables = 'ID', .fun = function(x){
+  coi_pattern = paste(table(x$Episode_Identifier), collapse = '_')
+  num_markers = sum(sapply(MSs, function(ms) any(!is.na(x[,ms]))))
+  data.frame(coi_pattern = coi_pattern, num_markers = num_markers)
+  })
+
+#-----------------------------------------------------------------
+# Lets visually inspect these data to see if we can find evidence of 
+# clones in the data that are not captured in the results
+#-----------------------------------------------------------------
+# Inspect simple first 
+ColorPlot_MSdata(MS_data = reformat_MSdata(MS_pooled[MS_pooled$ID %in% IDs_prob_simple,])) # Plot of all data 
+X = Results_MaxHap20[Results_MaxHap20$Phased != 'D_D', ] # 5 different IDs (501 at top of plot)
+X[nrow(X):1,] # Reverse order to match plot
+# VHX_501 results make sense
+# VHX_352 results make sense
+# VHX_239 results make sense
+# BPD_598 results make sense
+# BPD_562 results make sense 
+
+# We need to process the results to check for inflated (not yet done)
+ColorPlot_MSdata(MS_data = reformat_MSdata(MS_pooled[MS_pooled$ID %in% IDs_prob_inflate,])) # Plot of all data 
