d3e9a5a50ba2c763b235e73f8b188dd14c7a40d6
artaylor85 <aimee.r.taylor.85@gmail.com>
2019-06-07 20:00:48 -0400

    Intermediate modifications

--- Genetic_Model/Setting_Max_Hap_genotypes_comb.R
+++ Genetic_Model/Setting_Max_Hap_genotypes_comb.R
@@ -1,32 +1,51 @@
 ##############################################################################################
-# This script was used to choose reasonable limits for Max_Hap_genotypes and Max_Hap_comb 
-# based on the BPD and VHX data. Note that, the code takes a long time when Max_Hap_comb is 
-# high as it needs to sum over a lot of graphs
+# Initially, this script was written to choose reasonable limits for both Max_Hap_genotypes 
+# and Max_Hap_comb based on the BPD and VHX data. Important because prob_post_CLI is slow
+# time when Max_Hap_comb is high as it needs to sum over a lot of graphs. 
+# It is now used to set Max_Hap_genotype, and Max_Hap_comb in combination with results
+# generated in Effect_probablistic_v_deterministic.R 
 #
-# If probablistic is faster than deterministic, may be preferable to set 
-# Max_Hap_genotypes to low, s.t. some that could be phased deterministically
+# ********************** !!!!IMPORTANT!!!! ******************************
+# 1) Not always true that no. compatible haploid genotypes > no. of compatible combinations
+# 2) Mapping between no. compatible haploid genotypes and combinations is NOT one-to-one 
+# 3) No. combinations does not necessarily scale with no. haploid genotypes compatible, e.g. 
+# BPD_402_1: 16 genotypes comp with 196 compatible combinations
+# VHX_501_1: 64 genotypes comp with 32 compatible combinations
+# ***********************************************************************
+#
+# Re 3), it thus makes little sense to set Max_Hap_comb based on Max_Hap_genotypes, e.g.
+# 97.5th percentile of no. of compatible haploid genotypes per episode BPD and VHX simple: 16 
+# max no. of hap. genotype combinations given 97.5th percentile: 196 
+# The 99th percentile, 50.24, is between 32 and 64, 
+# for which max no. of hap. genotype combinations are 32 and 64 (less than for 16)
+#
+# Nevertheless, initial approach: set Max_Hap_genotypes to highest reasonable, 
+# then set Max_Hap_comb to match highest in the set that pass Max_Hap_genotypes
+# (for Max_Hap_genotypes = 72, highest combinations: 1296)
+# However, setting Max_hap_comb > 1000 is prohibitively slow when applied to many episodes. 
+# If we keep Max_Hap_genotypes = 100, we will deterministically phase all with <= Max_Hap_genotypes 
+# inc. VHX_461_1 with 72 comp genotypes and 1296 possible combinations. 
+# For all with > Max_Hap_genotypes we phase probabilistically with Max_Hap_comb combinations. 
+#
+# If probablistic faster than deterministic, preferable to set 
+# Max_Hap_genotypes to low in the main code, s.t. some that could be phased deterministically
 # are phased probabilistically but with Max_Hap_comb set to sufficiently high
 # to capture all that could be otherwise phased deterministically
-#
 # Doing so requires at least one high Max_Hap_genotypes run
 # to show that deterministically phased is the same as probablistically phased. 
-#
-#
-# Based on this script: 
-# 97.5th percentile of no. of haploid genotypes per episode: 64
-# 99th percentile of no. of haploid genotypes per episode: 128
-# Max number of hap. genotype combinations given 97.5th percentile: 216
-# Max number of hap. genotype combinations given 99th percentile: 1296
-#
-# However, Setting Max_hap_comb > 1000 can lead to UpperComplexity > 10^6
-# Combinations is excessively slow when nrow(Hnt) = 288 choose cnt = 4 (280720440 rows)
-#
-# In clonclusion
-# Let's set Max_Hap_genotypes = 100 for deterministic
-# Let's set Max_Hap_comb = 800 always
+# 
+# From Effect_probablistic_v_deterministic.R, when Max_Hap_genotypes = 100 and...
+# Max_Hap_comb = 800, deterministically faster and only 5 not "D_D" (all in inflated data set)
+# Max_Hap_comb = 300, ? faster ann how many fewer inflated data set have P1
 ##############################################################################################
-
 rm(list = ls())
+load('../RData/RPackages_List.RData')
+new.pkg <- pkgs[!(pkgs %in% installed.packages()[, "Package"])]
+if(length(new.pkg) > 0){ # Install using .R script
+  stop('Please run the script Install_and_load_required_packages.R before returning to this script. Thanks.')}else{
+    sapply(pkgs, require, character.only = TRUE) # Load all packages
+  }
+
 load('../RData/GeneticModel/MS_data_PooledAnalysis.RData') # For MS_pooled
 load('../RData/Data_for_relatedness.RData') # For Fs_Combined
 source('../Genetic_Model/iGraph_functions.R')
@@ -34,7 +53,7 @@ source('../Genetic_Model/Likelihood_function.R')
 source('../Genetic_Model/Data_functions.R')
 source('../Genetic_Model/test_Rn_compatible.R')
 source('../Genetic_Model/post_prob_CLI.R')
-source('../Genetic_Model/hap_combinations.R')
+source('../Genetic_Model/hap_combinations_functions.R')
 source('../Simulation_Study/BuildSimData.R')
 
 
@@ -67,23 +86,30 @@ n_haps_per_episode = lapply(yns, function(yn){ # For a given individual
     num_haps = prod(ynmt_size) # Calculate the number of haploid genotypes
   })
 })
-names(n_haps_per_episode) = NULL # Remove IDs since episode ID sufficient 
-n_haps_per_episode = unlist(n_haps_per_episode) # Convert to vector and then string
-PROB = 0.975
-Percentile = 128 # quantile(n_haps_per_episode, probs = PROB)
+
+
+# Remove complex and no recurrence
+complex = (Tns > Max_Eps) | (sum_cns > Max_Tot_Vtx) # 54 indiv. whose data are too complex 
+no_recurrence = Tns < 2 # individuals that have Tn = 1 (no recurrence)
+IDs = IDs_all[(!complex & !no_recurrence)] 
+N = length(IDs)
+
+# Number of genotypes before and after removing complex
+Num_with_wout = cbind("WithComplex" = tail(sort(unlist(n_haps_per_episode)),10), 
+      "WOutComplex" = tail(sort(unlist(n_haps_per_episode[IDs])),10)) 
+Num_with_wout
+
+
+
+Limit = 72 # Limit (3rd hightest after removal of complex)
 
 
 #==========================================================================
 # Set limit on haploid genotypes
 #==========================================================================
-Max_Hap_genotypes = Percentile # Set max to 95th percentil Limits based on script
+Max_Hap_genotypes = Limit # Set max to 95th percentil Limits based on script
 number_comp = number_geno = array(dim = length(Episodes), dimnames = list(Episodes)) # Includes all
 
-# Loop over not too complex
-complex = (Tns > Max_Eps) | (sum_cns > Max_Tot_Vtx) # 54 indiv. whose data are too complex 
-no_recurrence = Tns < 2 # individuals that have Tn = 1 (no recurrence)
-IDs = IDs_all[(!complex & !no_recurrence)] 
-N = length(IDs)
 
 for(i in 1:N){
   
@@ -126,19 +152,27 @@ for(i in 1:N){
 }
 
 
-
-
-# Condense
+# Remove skipped due to total_haps_count > Max_Hap_genotypes
 number_comp = number_comp[!is.na(number_comp)]
 number_geno = number_geno[!is.na(number_geno)]
-max(number_comp)
-max(number_geno[names(number_comp)])
-all(number_geno == n_haps_per_episode[names(number_geno)]) # Check the same answer: yes
 
+# Extract percentile before removing ID names
+PROB = 0.99
+Percentile = quantile(unlist(n_haps_per_episode[IDs]), probs = PROB)
+names(n_haps_per_episode) = NULL # Remove id names (for next step)
+
+# Check no. haploid genotypes compatible calculation gives same result as enumeration
+all(number_geno == unlist(n_haps_per_episode)[names(number_geno)]) 
+
+# Inspect relationship between no. haploid genotypes compatible and 
+# no. of combinations of haploid genotypes compatible
+cbind('No_geno' = number_geno[names(sort(number_comp))], 
+      'No_geno_comb' = sort(number_comp))
 plot(y = number_comp, x = number_geno[names(number_comp)])
 
+# Report precentile
 writeLines(sprintf('%sth percentile of no. of haploid genotypes per episode: %s', PROB*100, Percentile))
-writeLines(sprintf('Max number of hap. genotype combinations given %sth percentile: %s', PROB*100, max(number_comp)))
+writeLines(sprintf('Max number of hap. genotype combinations given limit of %s: %s', Limit, max(number_comp)))
 
 
 
